60a38768ae40850dc44e62bb494e88d50688a76d
Robert Ende <robert.ende@web.de>
2018-01-19 05:55:16 +0100

    testing express compression + minify

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const compression = require("compression");
+const minify = require("express-minify");
+
 const PORT = process.env.PORT || 5000;
 
 express()
-    .use(express.static(path.join(__dirname, 'public')))
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  .use(compression())
+  .use(minify())
+  .use(express.static(path.join(__dirname, "public")))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
