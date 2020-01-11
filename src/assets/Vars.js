923cb42faad0b7bbced4f2cf255469cac45f18b5
Robert Ende <robert@ende.pro>
2020-01-11 16:46:06 +0100

    fix: updated PostScriptum mortar ranges

--- src/assets/Vars.js
+++ src/assets/Vars.js
@@ -41,20 +41,20 @@ export const SQUAD_VELOCITY = 109.890938;
 export const SQUAD_MAX_DISTANCE = 1232;
 
 export const PS_8CM_NAME = "GER 8cm";
-export const PS_8CM_VELOCITY = 108.395845;
-export const PS_8CM_MAX_DISTANCE = 1198;
+export const PS_8CM_VELOCITY = 105.762785;
+export const PS_8CM_MAX_DISTANCE = 1141;
 
 export const PS_3INCH_NAME = "BRIT 3″";
-export const PS_3INCH_VELOCITY = 119.874256;
-export const PS_3INCH_MAX_DISTANCE = 1466;
+export const PS_3INCH_VELOCITY = 116.604952;
+export const PS_3INCH_MAX_DISTANCE = 1387;
 
 export const PS_4INCH_NAME = "BRIT 4″";
-export const PS_4INCH_VELOCITY = 159.666038;
-export const PS_4INCH_MAX_DISTANCE = 2601;
+export const PS_4INCH_VELOCITY = 155.244413;
+export const PS_4INCH_MAX_DISTANCE = 2459;
 
 export const PS_6CM_NAME = "US 6cm";
-export const PS_6CM_VELOCITY = 119.874256;
-export const PS_6CM_MAX_DISTANCE = 1466;
+export const PS_6CM_VELOCITY = 116.604952;
+export const PS_6CM_MAX_DISTANCE = 1387;
 
 export const GRAVITY = 9.8;
 export const MIL_TO_DEG_FACTOR = 360 / 6400;
