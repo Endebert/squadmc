3538f893b1ae25226cf14b4f08be78a109efd338
ansarto <68611454+ansarto@users.noreply.github.com>
2020-07-31 08:34:07 +0200

    fix(PostScriptum): update mortar velocities

--- src/assets/Vars.js
+++ src/assets/Vars.js
@@ -41,23 +41,23 @@ export const SQUAD_VELOCITY = 109.890938;
 export const SQUAD_MAX_DISTANCE = 1232;
 
 export const PS_8CM_NAME = "GER 8cm";
-export const PS_8CM_VELOCITY = 116.179939;
-export const PS_8CM_MAX_DISTANCE = 1377;
+export const PS_8CM_VELOCITY = 119.93;
+export const PS_8CM_MAX_DISTANCE = 1467;
 
 export const PS_3INCH_NAME = "BRIT 3″";
-export const PS_3INCH_VELOCITY = 116.179939;
-export const PS_3INCH_MAX_DISTANCE = 1377;
+export const PS_3INCH_VELOCITY = 119.93;
+export const PS_3INCH_MAX_DISTANCE = 1467;
 
 export const PS_4INCH_NAME = "BRIT 4″";
-export const PS_4INCH_VELOCITY = 160.598197;
-export const PS_4INCH_MAX_DISTANCE = 2632;
+export const PS_4INCH_VELOCITY = 166.94;
+export const PS_4INCH_MAX_DISTANCE = 2818;
 
 export const PS_6CM_NAME = "US 6cm";
-export const PS_6CM_VELOCITY = 116.179939;
-export const PS_6CM_MAX_DISTANCE = 1377;
+export const PS_6CM_VELOCITY = 119.93;
+export const PS_6CM_MAX_DISTANCE = 1467;
 
 export const GRAVITY = 9.8;
-export const MIL_TO_DEG_FACTOR = 360 / 6400;
+export const MIL_TO_DEG_FACTOR = 360.0 / 6283.1853;
 export const MAX_SUBTARGETS_COUNT = 49;
 
 /**
