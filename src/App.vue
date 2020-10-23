4dc927e6a0d01ac987b7dc85c5cf4fb16d122529
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-10-23 02:33:56 +0200

    Stop mistreating this.$router.options

--- src/App.vue
+++ src/App.vue
@@ -31,24 +31,22 @@ export default {
     // dynamically insert map routes
     mapData.init(baseUrl).then(() => {
       const mapNames = mapData.getMapNames();
-      mapNames.forEach((mapName) => {
-        this.$router.options.routes.push({
-          name: mapName,
-          path: `/${mapName}`,
-          component: () => import(/* webpackChunkName: "map" */ "./Map.vue"),
-          loading: Loading,
-          props: {
-            mapData,
-            postScriptum,
-            initialMap: mapName,
-          },
-        });
-        // fallback to first map, if name in route could not be matched
-        this.$router.options.routes.push({
-          path: "/*", redirect: mapNames[0],
-        });
+      const routes = mapNames.map(mapName => ({
+        name: mapName,
+        path: `/${mapName}`,
+        component: () => import(/* webpackChunkName: "map" */ "./Map.vue"),
+        loading: Loading,
+        props: {
+          mapData,
+          postScriptum,
+          initialMap: mapName,
+        },
+      }));
+      // fallback to first map, if name in route could not be matched
+      routes.push({
+        path: "/*", redirect: mapNames[0],
       });
-      this.$router.addRoutes(this.$router.options.routes);
+      this.$router.addRoutes(routes);
     });
   },
 };
